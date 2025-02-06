57bb30a648775396bc3ec7a7f0d2ab879e4da758
Tilman Holube <tilman@holube.de>
2025-02-06 10:11:08 +0100

    Adding SpringDocConfiguration to configuration scanning

--- src/main/java/de/uol/pgdoener/th1/Th1Application.java
+++ src/main/java/de/uol/pgdoener/th1/Th1Application.java
@@ -1,9 +1,10 @@
 package de.uol.pgdoener.th1;
 
+import org.openapitools.configuration.SpringDocConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {Th1Application.class, SpringDocConfiguration.class})
 public class Th1Application {
     public static void main(String[] args) {
         //System.out.println(org.hibernate.Version.getVersionString());
