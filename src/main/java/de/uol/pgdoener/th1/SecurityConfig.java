dc95a4ed381e3fbdd1ea1297a174b8d6b99bab0a
Jonas Pohl <jonas.jp4c@gmail.com>
2025-04-03 14:47:06 +0200

    Update SecurityConfig.java (#77)

--- src/main/java/de/uol/pgdoener/th1/SecurityConfig.java
+++ src/main/java/de/uol/pgdoener/th1/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://pg-doener-prod.virt.uni-oldenburg.de"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
