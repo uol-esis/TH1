94ea5dfc904559a062ac378faa30b178a425b5b5
Tilman Holube <tilman@holube.de>
2025-04-08 17:57:01 +0200

    provide equals hashCode and toString implementations to include array contents

--- src/main/java/de/uol/pgdoener/th1/business/infrastructure/converterchain/core/structures/RemoveColumnByIndexStructure.java
+++ src/main/java/de/uol/pgdoener/th1/business/infrastructure/converterchain/core/structures/RemoveColumnByIndexStructure.java
@@ -1,6 +1,31 @@
 package de.uol.pgdoener.th1.business.infrastructure.converterchain.core.structures;
 
+import lombok.NonNull;
+
+import java.util.Arrays;
+
 public record RemoveColumnByIndexStructure(
-        Integer[] columns
+        @NonNull Integer[] columns
 ) implements IStructure {
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RemoveColumnByIndexStructure that = (RemoveColumnByIndexStructure) o;
+        return Arrays.equals(columns, that.columns);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(columns);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoveColumnByIndexStructure{" +
+                "columns=" + Arrays.toString(columns) +
+                '}';
+    }
+
 }
