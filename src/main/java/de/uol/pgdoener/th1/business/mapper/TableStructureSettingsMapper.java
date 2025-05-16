10e4506bd7e2514f1df064af986a159146b19a92
MariusOptazi <121859766+MariusOptazi@users.noreply.github.com>
2025-05-16 14:12:21 +0200

    change pom

--- src/main/java/de/uol/pgdoener/th1/business/mapper/TableStructureSettingsMapper.java
+++ src/main/java/de/uol/pgdoener/th1/business/mapper/TableStructureSettingsMapper.java
@@ -1,8 +1,12 @@
 package de.uol.pgdoener.th1.business.mapper;
 
+
 import de.uol.pgdoener.th1.business.dto.TableStructureGenerationSettingsDto;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.Optional;
+
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TableStructureSettingsMapper {
@@ -11,8 +15,8 @@ public class TableStructureSettingsMapper {
         TableStructureGenerationSettingsDto settings = new TableStructureGenerationSettingsDto();
 
         if (settings.getRemoveHeader().isPresent()) {
-            
-        }
 
+        }
+        return settings;
     }
 }
