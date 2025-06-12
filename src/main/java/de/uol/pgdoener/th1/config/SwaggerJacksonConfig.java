a1f7b7493dafa862d5c4ba45ab0d20c3ecc80fc9
Tilman Holube <tilman@holube.de>
2025-06-12 10:28:47 +0200

    switch to the correct ObjectMapperProvider

--- src/main/java/de/uol/pgdoener/th1/config/SwaggerJacksonConfig.java
+++ src/main/java/de/uol/pgdoener/th1/config/SwaggerJacksonConfig.java
@@ -1,16 +1,20 @@
 package de.uol.pgdoener.th1.config;
 
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import io.swagger.v3.core.util.Json31;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.providers.ObjectMapperProvider;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerJacksonConfig {
+
+    private final ObjectMapperProvider objectMapperProvider;
 
     @PostConstruct
     public void init() {
-        Json31.mapper().registerModule(new Jdk8Module());
+        objectMapperProvider.jsonMapper().registerModule(new Jdk8Module());
     }
 
 }
