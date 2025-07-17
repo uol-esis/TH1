58546a3380ad86bf6e584ac5d68a07ff4c70516a
Jonas Pohl <jonas.jp4c@gmail.com>
2025-07-17 16:13:07 +0200

    Allowing to disable objectstorage

--- src/main/java/de/uol/pgdoener/th1/config/S3Config.java
+++ src/main/java/de/uol/pgdoener/th1/config/S3Config.java
@@ -1,23 +1,25 @@
 package de.uol.pgdoener.th1.config;
 
-import de.uol.pgdoener.th1.autoconfigure.S3Properties;
+import de.uol.pgdoener.th1.autoconfigure.ObjectStorageProperties;
 import io.minio.MinioClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@ConditionalOnProperty(value = "th1.objectstorage.type", havingValue = "s3")
 public class S3Config {
 
-    private final S3Properties s3Properties;
+    private final ObjectStorageProperties osProperties;
 
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint(s3Properties.getUrl())
-                .credentials(s3Properties.getAccessKey(), s3Properties.getSecretKey())
-                .region(s3Properties.getRegion())
+                .endpoint(osProperties.getS3().getUrl())
+                .credentials(osProperties.getS3().getAccessKey(), osProperties.getS3().getSecretKey())
+                .region(osProperties.getS3().getRegion())
                 .build();
     }
 }
--- src/main/java/de/uol/pgdoener/th1/storage/impl/S3StorageService.java
+++ src/main/java/de/uol/pgdoener/th1/storage/impl/S3StorageService.java
@@ -1,6 +1,6 @@
 package de.uol.pgdoener.th1.storage.impl;
 
-import de.uol.pgdoener.th1.autoconfigure.S3Properties;
+import de.uol.pgdoener.th1.autoconfigure.ObjectStorageProperties;
 import de.uol.pgdoener.th1.storage.StorageService;
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
@@ -8,6 +8,7 @@ import io.minio.PutObjectArgs;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
@@ -20,9 +21,10 @@ import java.util.UUID;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class MinioStorageService implements StorageService {
+@ConditionalOnProperty(value = "th1.objectstorage.type", havingValue = "s3")
+public class S3StorageService implements StorageService {
 
-    private final S3Properties s3Properties;
+    private final ObjectStorageProperties objectStorageProperties;
 
 
     private final MinioClient minioClient;
@@ -32,7 +34,7 @@ public class MinioStorageService implements StorageService {
         UUID objectID = UUID.randomUUID();
         try {
             minioClient.putObject(PutObjectArgs.builder()
-                    .bucket(s3Properties.getBucket().getName())
+                    .bucket(objectStorageProperties.getS3().getBucket().getName())
                     .object(objectID.toString())
                     .stream(inputStream, inputStream.available(), -1)
                     .build());
@@ -48,7 +50,7 @@ public class MinioStorageService implements StorageService {
     public Optional<InputStream> load(UUID objectID) {
         try {
             var response = minioClient.getObject(GetObjectArgs.builder()
-                    .bucket(s3Properties.getBucket().getName())
+                    .bucket(objectStorageProperties.getS3().getBucket().getName())
                     .object(objectID.toString())
                     .build());
             return Optional.of(response);
