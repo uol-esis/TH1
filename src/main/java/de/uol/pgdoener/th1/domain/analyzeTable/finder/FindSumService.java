69a75fb214d70e0a091784c632f0a74736fc0c45
Tilman Holube <tilman@holube.de>
2025-08-14 23:50:37 +0200

    Further optimizations
    - switched to virtual threads in spring
    - avoided array copies in CsvParsingService
    - merge patterns in FindSumService

--- src/main/java/de/uol/pgdoener/th1/domain/analyzeTable/finder/FindSumService.java
+++ src/main/java/de/uol/pgdoener/th1/domain/analyzeTable/finder/FindSumService.java
@@ -21,10 +21,14 @@ import java.util.regex.Pattern;
 public class FindSumService {
 
     public Optional<SumReportDto> find(MatrixInfo matrixInfo, String[][] matrix, List<String> blockList) {
-        List<Predicate<String>> patterns = preparePatterns(blockList);
+        if (blockList == null || blockList.isEmpty()) {
+            log.info("Block list is empty, no sums to find.");
+            return Optional.empty();
+        }
+        Predicate<String> pattern = preparePatterns(blockList);
 
-        List<Integer> columnsWithSum = getColumnsWithSum(matrixInfo, matrix, patterns);
-        List<Integer> rowsWithSum = getRowsSum(matrixInfo, matrix, patterns);
+        List<Integer> columnsWithSum = getColumnsWithSum(matrixInfo, matrix, pattern);
+        List<Integer> rowsWithSum = getRowsSum(matrixInfo, matrix, pattern);
 
         if (columnsWithSum.isEmpty() && rowsWithSum.isEmpty())
             return Optional.empty();
@@ -35,33 +39,35 @@ public class FindSumService {
         return Optional.of(sumReport);
     }
 
-    private List<Predicate<String>> preparePatterns(List<String> blockList) {
-        return blockList.stream()
-                .map(value -> Pattern.compile("^" + Pattern.quote(value) + "\\b.*"))
-                .map(Pattern::asPredicate)
-                .toList();
+    private Predicate<String> preparePatterns(List<String> blockList) {
+        StringBuilder sb = new StringBuilder();
+        for (String value : blockList) {
+            sb.append("(^").append(Pattern.quote(value)).append("\\b.*)|");
+        }
+        String pattern = sb.toString();
+        return Pattern.compile(pattern.substring(0, pattern.length() - 1)).asPredicate();
     }
 
-    private List<Integer> getColumnsWithSum(MatrixInfo matrixInfo, String[][] matrix, List<Predicate<String>> patterns) {
+    private List<Integer> getColumnsWithSum(MatrixInfo matrixInfo, String[][] matrix, Predicate<String> pattern) {
         List<ColumnInfo> columnInfos = matrixInfo.columnInfos();
         List<Integer> columnsWithSum = new ArrayList<>();
         for (ColumnInfo columnInfo : columnInfos) {
             CellInfo firstCell = columnInfo.cellInfos().getFirst();
             String entry = matrix[firstCell.rowIndex()][firstCell.columnIndex()];
 
-            if (isInBlockList(entry, patterns)) columnsWithSum.add(firstCell.columnIndex());
+            if (isInBlockList(entry, pattern)) columnsWithSum.add(firstCell.columnIndex());
         }
         return columnsWithSum;
     }
 
-    private List<Integer> getRowsSum(MatrixInfo matrixInfo, String[][] matrix, List<Predicate<String>> patterns) {
+    private List<Integer> getRowsSum(MatrixInfo matrixInfo, String[][] matrix, Predicate<String> pattern) {
         return matrixInfo.rowInfos().stream()
                 .parallel() // Remove to handle longer burst loads
                 .filter(rowInfo -> {
                             for (CellInfo cellInfo : rowInfo.cellInfos()) {
                                 String entry = matrix[cellInfo.rowIndex()][cellInfo.columnIndex()];
                                 entry = entry.toLowerCase();
-                                if (isInBlockList(entry, patterns)) {
+                                if (isInBlockList(entry, pattern)) {
                                     return true; // Found a cell that matches the block list
                                 }
                             }
@@ -72,13 +78,8 @@ public class FindSumService {
                 .toList();
     }
 
-    private boolean isInBlockList(String entry, List<Predicate<String>> patterns) {
-        for (Predicate<String> pattern : patterns) {
-            if (pattern.test(entry)) {
-                return true;
-            }
-        }
-        return false;
+    private boolean isInBlockList(String entry, Predicate<String> pattern) {
+        return pattern.test(entry);
     }
 
 }
