8c09884673b3bac54681d674a4989ec516618760
MariusOptazi <121859766+MariusOptazi@users.noreply.github.com>
2025-09-18 15:00:08 +0200

    rework logic
    add test

--- src/main/java/de/uol/pgdoener/th1/domain/fileprocessing/helper/TypeDetector.java
+++ src/main/java/de/uol/pgdoener/th1/domain/fileprocessing/helper/TypeDetector.java
@@ -34,9 +34,9 @@ public class TypeDetector {
 
         if (isTimestamp(raw, length)) return ValueType.TIMESTAMP;
 
-        if (isText(raw)) return ValueType.TEXT;
+        if (isNumber(raw)) return ValueType.NUMBER;
 
-        return ValueType.NUMBER;
+        return ValueType.TEXT;
     }
 
     private boolean isDate(String s, int length) {
@@ -59,18 +59,20 @@ public class TypeDetector {
         return TIMESTAMP_PATTERN.matcher(s).matches();
     }
 
-    private boolean isText(String s) {
+    private boolean isNumber(String s) {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (Character.isLetter(c) || isSeparator(c, i)) return true;
+            if (Character.isWhitespace(c) || c == '\u00A0') continue;
+            if (Character.isDigit(c)) continue;
+            if (!isNumberSeparator(c, i)) return false;
         }
-        return false;
+        return true;
     }
 
-    private boolean isSeparator(char c, int idx) {
+    private boolean isNumberSeparator(char c, int idx) {
         return switch (c) {
-            case '/', '|', ':' -> true;
-            case '-' -> idx != 0;
+            case '.', ',', '\'', '€', '$', '%' -> idx != 0;
+            case '-', '+' -> idx == 0;
             default -> false;
         };
     }
